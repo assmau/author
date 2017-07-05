87ae25134fd6b3c46327ba57de1d3e3363857187
assmau <orhandavidov@gmail.com>
2017-07-05 18:33:53 +0300

    added section team, section plan and section contact

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,20 @@
 $(function() {
 
-	$(".main-menu").on("click","a", function (event) {
-	    event.preventDefault();
-	    var id  = $(this).attr('href'),
-	        top = $(id).offset().top;
-	    $('body,html').animate({scrollTop: top}, 1500);
-	});
+	// $(".main-menu").on("click","a", function (event) {
+	//     event.preventDefault();
+	//     var id  = $(this).attr('href'),
+	//         top = $(id).offset().top;
+	//     $('body,html').animate({scrollTop: top}, 1500);
+	// });
+
+
+	$('a.page-scroll').click(function() {
+ 		var $anchor = $(this);
+ 			$('body').animate({
+             	scrollTop: ($($anchor.attr('href')).offset().top - 100)
+         	}, 600);
+         event.preventDefault();
+ 	});
 
   		$('.bxslider').bxSlider();
 });
